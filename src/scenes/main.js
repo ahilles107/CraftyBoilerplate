6f0a820720f6f867d5ae67e338d5112c19a0876f
Piotr Radzikowski <piotr.radzikowski@strazpozarna.org.pl>
2012-04-16 16:00:00 +0200

    correct structure, add interface entity

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -2,11 +2,13 @@ Crafty.scene("main", function() {
 
 	var elements = [
         "src/entities/ufo.js",
+        "src/interfaces/info.js"
 	];
 	
 	//when everything is loaded, run the main scene
 	require(elements, function() {	   
 		sc['ufo'] = new Ufo();
+		infc['info'] = new Info();
 	});
 
 });
